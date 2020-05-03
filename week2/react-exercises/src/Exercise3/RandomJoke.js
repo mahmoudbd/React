c0a33e1d8c5697b0ce430e91bbcadc7cc390804c
Mahmoud Badran <mahmoudbd@gmail.com>
2020-05-04 00:42:37 +0200

    handel erro & handel loading

--- week2/react-exercises/src/Exercise3/RandomJoke.js
+++ week2/react-exercises/src/Exercise3/RandomJoke.js
@@ -1,21 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import Joke from './Joke';
 import '../App.css';
-import axios from 'axios';
 
 const RandomJoke = () => {
 	const [ joke, setJoke ] = useState({});
+	const [ hasError, setError ] = useState(false);
+	const [ isLoading, setLoading ] = useState(false);
 	useEffect(() => {
-		axios
-			.get('https://official-joke-api.appspot.com/random_joke')
-			.then((res) => {
-				setJoke(res.data);
-			})
-			.catch((err) => console.log('Error', err));
+		setLoading(true);
+		const getData = async () => {
+			try {
+				const response = await fetch('https://official-joke-api.appspot.com/random_joke');
+				const data = await response.json();
+				setJoke(data);
+				setLoading(false);
+			} catch (err) {
+				console.log(err);
+				setLoading(false);
+				setError(true);
+			}
+		};
+		getData();
 	}, []);
+
 	return (
 		<div className="Joke">
-			<Joke setup={joke.setup} punchline={joke.punchline} />
+			{hasError && <h2>Something went wrong...</h2>}
+			{isLoading && <h2>Loading...</h2>}
+			{!hasError && <Joke setup={joke.setup} punchline={joke.punchline} />}
 		</div>
 	);
 };
